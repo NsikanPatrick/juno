8eb1b5bd95be1df2a5c7dd231929ee0728c0cf83
NsikanPatrick <nsikanpatrick69@gmail.com>
2025-03-24 13:51:42 +0100

    Deployed the generative App instead of conversational

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'; // Conversational AI
-// import App from './Generative/App.jsx'; // Generative AI - Analyses texts, images, links
+// import App from './App.jsx'; // Conversational AI
+import App from './Generative/App.jsx'; // Generative AI - Analyses texts, images, links
 // import App from './ImageGeneration/App.jsx'; // Image generation AI
 
 createRoot(document.getElementById('root')).render(
