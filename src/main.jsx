4acc0a31176ffe38101605802a1bd103bfa0877e
NsikanPatrick <nsikanpatrick69@gmail.com>
2025-03-26 16:28:29 +0100

    Added environment variables file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'; // Conversational AI
 import App from './Generative/App.jsx'; // Generative AI - Analyses texts, images, links
-// import App from './ImageGeneration/App.jsx'; // Image generation AI
+// import App from './ImageGeneration/App.jsx'; // Image generation AI. This has a few issues at the moment
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
